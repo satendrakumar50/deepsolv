ed96fae305ffab4d40925ae1d2b51d51289312e5
satendrakumar50 <gk1293123@gmail.com>
2026-05-01 16:07:59 +0530

    fix build1

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,28 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { Auth0Provider } from "@auth0/auth0-react";
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import './index.css'
+// import App from './App.jsx'
+// // import { Auth0Provider } from "@auth0/auth0-react";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-<Auth0Provider
-      domain="dev-n68jj8mppq47pxs8.us.auth0.com"
-      clientId="McNiisiYsA8QidVbUvmbflshehaUu69g"
-      authorizationParams={{
-        redirect_uri: window.location.origin
-      }}
-    >
-      <App />
-    </Auth0Provider>
-  </StrictMode>,
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+// <Auth0Provider
+//       domain="dev-n68jj8mppq47pxs8.us.auth0.com"
+//       clientId="McNiisiYsA8QidVbUvmbflshehaUu69g"
+//       authorizationParams={{
+//         redirect_uri: window.location.origin
+//       }}
+//     >
+//       <App />
+//     </Auth0Provider>
+//   </StrictMode>,
+// )
+
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <App />
 )
